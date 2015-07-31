3f83066db3dd38201271aa95be87a11d2b76a1f5
jose <jlceciliano@gmail.com>
2015-08-01 01:33:15 +0200

    Paso 16: atenticacion y sesion

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 // var users = require('./routes/users'); // Quitamos la routa a users
@@ -23,9 +24,25 @@ app.use(favicon(__dirname + '/public/favicon.ico')); // agregado favicon
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Helpers dinamicos:
+app.use(function(req, res, next) {
+
+// guardar path en session.redir para despues de login
+    if (!req.path.match(/\/login|\/logout/)) {
+        console.log ("------------ app.js ----" + req.path)
+        req.session.redir = req.path;
+    }
+
+// Hacer visible req.session en las vistas
+    res.locals.session = req.session;
+    next();
+    });
+
 
 app.use('/', routes);
 // app.use('/users', users); // Quitamos la routa a users
