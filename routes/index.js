547894fd0f5a9dad8ff9aabe144b387363b8a0d1
jose <jlceciliano@gmail.com>
2015-08-02 20:35:08 +0200

    Paso17: Autorizacion

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,15 @@ router.get('/', function(req, res) {
 router.param ('quizId', quizController.load); //autoload quizID
 
 // Definición de rutas de sesion
-router.get('/login',  sessionController.new);     // formulario login
-router.post('/login', sessionController.create);  // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
+router.get    ('/login',  sessionController.new);     // formulario login
+router.post   ('/login', sessionController.create);  // crear sesión
+router.get    ('/logout', sessionController.destroy); // destruir sesión
+router.get    ('/quizes/new'				,sessionController.loginRequired, quizController.new);
+router.post   ('/quizes/create'				,sessionController.loginRequired, quizController.create);
+router.get    ('/quizes/:quizId(\\d+)/edit' ,sessionController.loginRequired, quizController.edit);
+router.put    ('/quizes/:quizId(\\d+)'	    ,sessionController.loginRequired, quizController.update);
+router.delete ('/quizes/:quizId(\\d+)'		,sessionController.loginRequired, quizController.destroy);
+
 
 router.get('/quizes'                       , quizController.index);
 router.get('/quizes/:quizId(\\d+)'         , quizController.show);
